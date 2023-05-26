449abfe83f48936f1d7a7e1a2c151646de8d33e1
LeoSallu <L.sallustio99@gmail.com>
2023-05-26 16:12:34 +0200

    feat:router-link-navbar

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -6,11 +6,20 @@ export default {
 
 <template>
     <header>
-        <nav class="navbar">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary p-3">
             <div class="container">
-                <a class="navbar-brand" href="#">
-                    Boolfolio
-                </a>
+                <a class="navbar-brand" href="#">Boolfolio</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <router-link :to="{name:'home'}" class="nav-link">Home</router-link>
+                        <router-link :to="{name:'about'}" class="nav-link">About Us</router-link>
+                        <router-link :to="{name:'projects'}" class="nav-link">Projects</router-link>
+                    </div>
+                </div>
             </div>
         </nav>
     </header>
@@ -23,6 +32,15 @@ export default {
     div {
         a {
             color: aliceblue;
+        }
+        div{
+            a{
+                color: aliceblue;
+               &:hover {
+                border-bottom: 3px solid #005379;
+                color: #005379;
+               }
+            }
         }
     }
 }
